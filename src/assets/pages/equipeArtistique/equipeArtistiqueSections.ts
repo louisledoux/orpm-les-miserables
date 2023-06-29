35e5b7da450833173b16f2089b5116eb570bb72b
Pauline299 <pauline.ledoux29@hotmail.fr>
2023-06-29 17:34:11 +0200

    fin page equipe artistique

--- src/assets/pages/equipeArtistique/equipeArtistiqueSections.ts
+++ src/assets/pages/equipeArtistique/equipeArtistiqueSections.ts
@@ -2,6 +2,9 @@ import hubert from '@/assets/pages/orchestre/images/hubert.jpg';
 import eric from '@/assets/pages/equipeArtistique/images/ericpauwels.jpg';
 import magali from '@/assets/pages/equipeArtistique/images/magali.jpg';
 import celine from '@/assets/pages/equipeArtistique/images/celine.jpg';
+import manon from '@/assets/pages/equipeArtistique/images/manon.jpg'
+import claire from '@/assets/pages/equipeArtistique/images/claire.png'
+import marie from '@/assets/pages/equipeArtistique/images/marie.jpg'
 import { SectionData } from '@/types/Section.type';
 
 const equipeArtistiqueSections: SectionData[] = [
@@ -67,6 +70,57 @@ const equipeArtistiqueSections: SectionData[] = [
     image: {
       image: celine,
       alt: 'Céline costumiere',
+      style: { width: '100%', height: '100%', borderRadius: '2px' },
+    },
+  },
+  {
+    key: 'Mise en scene Manon',
+    pages: [{
+      title: 'Manon Souames\n Metteuse en scène',
+      paragraphs: [
+        "À l'âge de 7 ans, j'ai débuté la danse classique (discipline que je n'ai jamais vraiment abandonnée), puis je suis devenue choriste à 8 ans dans deux ensembles.",
+        "C'est à mes 18 ans que j'ai intégré une troupe de théâtre et découvert l'importance du jeu et de la mise en scène.",
+        "La comédie musicale est une révélation dès 2015 avec le projet Billy Elliot de l'ORPM : une scène, de la danse, du chant et de la comédie. Une évidence !",
+        "La mise en scène d'un tel spectacle est un vrai challenge artisitique et quand on m'a proposé ce défi complètement dingue, je n'ai pas mis longtemps à me décider. vivre cette expérience dans une troupe aussi dynamique et bienveillante est une occasion en or.",
+      ],
+    }],
+    image: {
+      image: manon,
+      alt: 'Manon metteuse en scèene',
+      style: { width: '100%', height: '100%', borderRadius: '2px' },
+    },
+  },
+  {
+    key: 'Mise en scene Claire',
+    pages: [{
+      title: 'Claire Gressier\n Metteuse en scène',
+      paragraphs: [
+        "Comédienne hors pair, j'ai toujours eu une facilité naturelle pour la comédie, si si demandez à mes soeurs et mes parents... Dotée d'une imagination débordante (peut-être un peu trop d'ailleurs) et d'une créativité sans limite, j'adore inventer des histoires et des mondes imaginaires. Je peux aussi transformer n'importe quoi en une oeuvre d'art.",
+        "Mes différentes expériences de comédie musicale ont su montrer et démontrer ma grande valeur ajoutée dans ces projets...",
+        "Bref, je pense être la moitié complémentaire parfaite de Manon pour s'éclater sur la mise en scène des Misérables. Toujours en route avec un projet un peu fou, l'ORPM c'est comme une deuxième famille !",
+        "C'est un nouveau défi à relever, un nouveau plaisir et surtout une nouvelle aventure musicale à partager. La comédie musicale Les Misérables est une oeuvre musicale iconique, avec des chansons emblématiques et des arrangements orchestraux incroyables.",
+      ],
+    }],
+    image: {
+      image: claire,
+      alt: 'Claire metteuse en scène',
+      style: { width: '100%', height: '100%', borderRadius: '2px' },
+    },
+  },
+  {
+    key: 'Chorégraphie Marie',
+    pages: [{
+      title: 'Marie Pioskowik\n Chorégraphe',
+      paragraphs: [
+        "Danseuse aguerrie",
+        "Formée professionnellement à la danse sur Paris dans les années 2000, je poursuis ma carrière en enseignant la danse moderne et contemporaine dans différentes structures de la métropole lilloise.",
+        "Mon style allie la gestuelle moderne dans une dynamique jazz. Auquel j'y ajoute une pédagogie d'expérience de presque 20 ans auprès des enfants et adultes.",
+        "Investie dans de nombreuses associations dans divers domaines, je découvre en 2020 l'ORPM, au travers de la comédie musicale «Memphis» La richesse et les valeurs du projet me plaisent instantanément. Mais c'est pour «Les Misérables» que je propose ma collaboration.",
+      ],
+    }],
+    image: {
+      image: marie,
+      alt: 'Marie chorégraphe',
       style: { width: '100%', height: '100%', borderRadius: '2px' },
     },
   },
