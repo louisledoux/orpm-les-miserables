eecc928f40386426da6acfedb03b84329c1b3d23
louisledoux <19322697+louisledoux@users.noreply.github.com>
2023-07-01 10:01:26 +0200

    chore: fix lint errors

--- src/assets/pages/precedentsSpectacles/precedentsSpectaclesSections.ts
+++ src/assets/pages/precedentsSpectacles/precedentsSpectaclesSections.ts
@@ -10,12 +10,14 @@ const precedentsSpectaclesSections: SectionData[] = [
       paragraphs: [
         "Au fil des années, nous avons eu l'immense privilège de partager notre passion avec un public de plus en plus large. En 2015, nous avons présenté “Billy Elliot” sur les scènes du Théâtre Sébastopol et dans toute la région, offrant à plus de 8000 personnes une expérience inoubliable.",
       ],
-      style: {padding: '130px 40px'},
+      style: { padding: '130px 40px' },
     }],
     image: {
       image: billyElliot,
       alt: 'Billy Elliot ORPM',
-      style: { width: '100%', minWidth: '550px', height: '100%', borderRadius: '2px', objectPosition: '50% 10%' },
+      style: {
+        width: '100%', minWidth: '550px', height: '100%', borderRadius: '2px', objectPosition: '50% 10%',
+      },
     },
   },
   {
@@ -24,12 +26,14 @@ const precedentsSpectaclesSections: SectionData[] = [
       paragraphs: [
         'En 2018-2019, nous avons continué à émerveiller les spectateurs avec notre comédie musicale “FAME”, un spectacle qui a rassemblé 11000 personnes au Colisée de Roubaix et toute l\'Euro-métropole.',
       ],
-      style: {padding: '130px 40px'},
+      style: { padding: '130px 40px' },
     }],
     image: {
       image: fame,
       alt: 'Fame ORPM',
-      style: { width: '100%', minWidth: '550px', height: '100%', borderRadius: '2px' },
+      style: {
+        width: '100%', minWidth: '550px', height: '100%', borderRadius: '2px',
+      },
     },
   },
   {
@@ -38,12 +42,14 @@ const precedentsSpectaclesSections: SectionData[] = [
       paragraphs: [
         "En 2022, nous avons relevé un nouveau défi: Emmener notre public faire un voyage à travers le temps à l'époque de la ségrégation raciale dans la ville de Memphis, Etats-Unis. Avec plus de 2 heures de spectacle, nous avons exploré le Blues, la Soul et le Rock'n Roll en compagnie des personnages de Huey Calhoun et de Felicia Farrell. Ce nouveau projet a attiré un public nombreux et enthousiaste qui a confirmé notre place dans la production de comédies musicales dans la tradition de celles de Londres ou de Broadway.",
       ],
-      style: {padding: '130px 40px'},
+      style: { padding: '130px 40px' },
     }],
     image: {
       image: memphis2,
       alt: 'Memphis ORPM',
-      style: { width: '100%', minWidth: '550px', height: '100%', borderRadius: '2px' },
+      style: {
+        width: '100%', minWidth: '550px', height: '100%', borderRadius: '2px',
+      },
     },
   },
 ];
