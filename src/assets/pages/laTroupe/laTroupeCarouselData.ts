46c50d0ef1c0be8fdd554ec8ef42dd3c265b8f78
Pauline299 <pauline.ledoux29@hotmail.fr>
2023-07-03 16:37:20 +0200

    feature: page la troupe

---
+++ src/assets/pages/laTroupe/laTroupeCarouselData.ts
@@ -0,0 +1,48 @@
+import { CarouselItemProps } from '@/components/CarouselItem/CarouselItem';
+import carousel1 from '@/assets/pages/laTroupe/images/carousel1.jpg';
+import carousel2 from '@/assets/pages/laTroupe/images/carousel2.jpg';
+import carousel3 from '@/assets/pages/laTroupe/images/carousel3.jpg';
+import carousel4 from '@/assets/pages/laTroupe/images/carousel4.jpg';
+import carousel5 from '@/assets/pages/laTroupe/images/carousel5.jpg';
+import carousel6 from '@/assets/pages/laTroupe/images/carousel6.jpg';
+import carousel7 from '@/assets/pages/laTroupe/images/carousel7.jpg';
+
+const laTroupeCarouselData: CarouselItemProps[] = [
+  {
+    imageSrc: carousel1,
+    alt: 'La troupe de l\'ORPM Memphis',
+    style: { objectPosition: 'bottom' },
+  },
+  {
+    imageSrc: carousel2,
+    alt: 'Yueh Memphis l\'ORPM',
+    style: { objectPosition: 'center' },
+  },
+  {
+    imageSrc: carousel3,
+    alt: 'La troupe de l\'ORPM en concert Memphis',
+    style: { objectPosition: 'center' },
+  },
+  {
+    imageSrc: carousel4,
+    alt: 'La troupe de l\'ORPM en concert Memphis',
+    style: { objectPosition: 'bottom' },
+  },
+  {
+    imageSrc: carousel5,
+    alt: 'La troupe de l\'ORPM en concert Memphis',
+    style: { objectPosition: 'center' },
+  },
+  {
+    imageSrc: carousel6,
+    alt: 'La troupe de l\'ORPM en concert Memphis',
+    style: { objectPosition: 'center' },
+  },
+  {
+    imageSrc: carousel7,
+    alt: 'La troupe de l\'ORPM en concert Memphis',
+    style: { objectPosition: 'center' },
+  },
+];
+
+export default laTroupeCarouselData;
