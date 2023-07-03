c11141de7441c22b8cf921b69972029f6d1de7dd
Pauline299 <pauline.ledoux29@hotmail.fr>
2023-07-03 23:32:23 +0200

    fix: responsive page comedie musicale

--- src/app/comedie-musicale/page.tsx
+++ src/app/comedie-musicale/page.tsx
@@ -18,13 +18,13 @@ export default function ComedieMusicale() {
           className="w-full h-full object-cover"
         />
       </div>
-      <div className="mb-120px">
+      <div className="lg:mb-120px mb-60px">
         <Typography.Title level={1}>
           La comédie musicale
         </Typography.Title>
       </div>
-      <div className="mb-60px text-center px-120px">
-        <Typography.Paragraph className="mb-2 !text-22">
+      <div className="mb-60px text-center lg:px-120px px-20px">
+        <Typography.Paragraph className="mb-2 !text-base lg:!text-22">
           Nous nous sommes lancé le défi de vous transporter dans les rues de Paris en 1832,
           à l&apos;époque des barricades, et de vous faire revivre les mélodies les plus célèbres
           de cette comédie musicale, revisitées pour séduire le public d&apos;aujourd&apos;hui.
