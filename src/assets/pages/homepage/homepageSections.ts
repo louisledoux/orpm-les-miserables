e2fc30b8bcc934c85879c58b4ea13c4ed5a0dc99
Pauline299 <pauline.ledoux29@hotmail.fr>
2023-07-04 17:35:50 +0200

    fix: home texts

--- src/assets/pages/homepage/homepageSections.ts
+++ src/assets/pages/homepage/homepageSections.ts
@@ -9,8 +9,7 @@ export const newsSection: SectionData = {
     paragraphs: [
       'Nouveau projet, nouveaux objectifs !',
       'Nous lançons une campagne de financement participatif pour financer les costumes de notre nouveau projet : Les Misérables ! Nous avons besoin de votre aide pour donner vie à cette expérience théâtrale mémorable, et créer des costumes à la hauteur de cette œuvre intemporelle ! Avec votre soutien, nous pourrons créer des costumes de qualité pour donner vie aux personnages emblématiques de cette œuvre et vous transporter dans l\'univers du 19e siècle.',
-      'Vous pouvez nous aider via le lien ci-dessous :',
-      'lien en attente',
+      'Vous pouvez nous aider en contribuant à notre projet, nous vous invitons à scroller en bas de page pour en savoir plus !',
       'Tous les dons sont sujets à déduction fiscale comme nous sommes une association (un don de 100€ ne vous coute que 34€ après déduction).',
       'Votre soutien, qu\'il soit financier ou par le biais de partages sur les réseaux sociaux, est inestimable pour nous et nous vous en sommes reconnaissants. Ensemble, nous allons atteindre nos objectifs pour créer une nouvelle expérience artistique incroyable !',
     ],
@@ -29,7 +28,7 @@ export const playSection: SectionData = {
       'Curieux de découvrir l\'univers des Misérables autrement ? Jouez avec nous, et aventurez-vous dans les méandres des oeuvres de Victor Hugo',
       'La page de jeux est en cours de construction, nous vous invitons à revenir très bientôt !',
       'Vous serez informés de l\'ouverture de la page et des animations ludiques via nos réseaux sociaux, n\'hésitez pas à nous suivre sur nos pages Facebook et Instagram pour ne rien râter de nos actualités !',
-      'En attendant, nous vous invitons à découvrir l\'histoire des Misérables ici et sa comédie musicale ici !',
+      'En attendant, nous vous invitons à découvrir l\'histoire des Misérables et sa comédie musicale dans les sections de notre site !',
       'Bonne lecture ! ',
     ],
   }],
