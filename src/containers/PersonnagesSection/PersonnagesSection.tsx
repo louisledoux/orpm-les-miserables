c8a965461bf2374f6aa493dbc60df97be44eda14
louisledoux <19322697+louisledoux@users.noreply.github.com>
2023-07-04 09:45:18 +0200

    fix(PersonnagesSection): fix reverse responsive conditions

--- src/containers/PersonnagesSection/PersonnagesSection.tsx
+++ src/containers/PersonnagesSection/PersonnagesSection.tsx
@@ -21,14 +21,14 @@ function PersonnagesSection({
   pages, image, reverse,
 }: SectionProps) {
   return (
-    <div className={`flex justify-between items-center px-20px lg:px-120px mb-60px lg:${reverse ? 'flex-row-reverse' : 'flex-row'} ${reverse ? 'flex-col' : 'flex-col'}`}>
+    <div className={`flex justify-between items-center px-20px lg:px-120px mb-60px ${reverse ? 'lg:flex-row-reverse' : 'lg:flex-row'} ${reverse ? 'flex-col' : 'flex-col'}`}>
       <div className={`w-full lg:max-w-33 h-353px mb-30px lg:mb-0 lg:h-full lg:${reverse ? 'ml-30px' : 'mr-30px'}`}>
         <Image className="object-cover object-top lg:object-center h-full w-full lg:max-h-550" src={image.image} alt={image.alt} style={image.style} />
       </div>
       {pages.map((page) => (
         <div
           key={page.title}
-          className={`flex flex-col lg:max-w-66 w-full lg:py-60px px-20px lg:px-40px lg:${reverse ? 'mr-30px' : 'ml-30px'}`}
+          className={`flex flex-col lg:max-w-66 w-full lg:py-60px px-20px lg:px-40px ${reverse ? 'lg:mr-30px' : 'lg:ml-30px'}`}
           style={page.style}
         >
           <Typography.Title className="mb-30px lg:mb-60px text-center" level={2}>{page.title}</Typography.Title>
