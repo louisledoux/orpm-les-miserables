81c811aaccc2c60aa011c2ec98446b42d500738f
louisledoux <19322697+louisledoux@users.noreply.github.com>
2023-07-06 13:19:05 +0200

    fix(metadata): fix wording

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const oswald = Oswald({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'ORPM Spectacle - Les Misérables',
-  description: 'Nous vous invitons à nous rejoindre pour une aventure captivante : Les Misérables d\'après l\'oeuvre de Victor Hugo ! Nous nous sommes lancé le défi de vous transporter dans les rues de Paris en 1832, à l\'époque des barricades, et de vous faire revivre les mélodies les plus célèbres de cette comédie musicale, revisitées pour séduire le public d\'aujourd\'hui.',
+  description: 'Nous vous invitons à nous rejoindre pour une aventure captivante : Les Misérables d\'après l\'oeuvre de Victor Hugo ! Nous nous sommes lancés le défi de vous transporter dans les rues de Paris en 1832, à l\'époque des barricades, et de vous faire revivre les mélodies les plus célèbres de cette comédie musicale, revisitées pour séduire le public d\'aujourd\'hui.',
 };
 
 export default function RootLayout({
