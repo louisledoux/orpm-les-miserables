1f7e22f4f50d79609028908223a6fb940dce5684
louisledoux <19322697+louisledoux@users.noreply.github.com>
2023-07-19 14:31:10 +0200

    fix(sitemap): fix sitemap urls

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,12 @@
 import { MetadataRoute } from 'next';
 import RoutesPathEnum from '@/routes/Routes.enum';
 
+const domain = 'https://www.orpm-les-miserables.fr';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = Object.values(RoutesPathEnum);
   return routes.map((route) => ({
-    url: route,
+    url: `${domain}${route}`,
     lastModified: new Date(),
   }));
 }
