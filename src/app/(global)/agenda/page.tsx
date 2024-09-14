f10e080731b29e4712a6eebcc5646bbf349ba2a2
Louis Ledoux <19322697+louisledoux@users.noreply.github.com>
2024-09-14 12:52:21 +0200

    feat(Agenda): remove header image

--- src/app/(global)/agenda/page.tsx
+++ src/app/(global)/agenda/page.tsx
@@ -1,28 +1,27 @@
-import { Metadata } from 'next';
+'use client';
+
 import Image from 'next/image';
 import agendaHeader from '@/assets/pages/agenda/images/agendaHeader.jpg';
 import Typography from '@/components/Typography/Typography';
 import React from 'react';
 import Agenda from '@/containers/Agenda/Agenda';
-
-export const metadata: Metadata = {
-  title: 'Agenda',
-  openGraph: {
-    title: 'Agenda',
-  },
-};
+import useViewport from '@/hooks/useViewport';
 
 export default function AgendaPage() {
+  const { isMobileScreen } = useViewport();
+
   return (
     <>
-      <div className="h-headerImage w-full">
-        <Image
-          src={agendaHeader}
-          alt="Cosette les Miserables ORPM"
-          style={{ objectPosition: '5% 35%' }}
-          className="w-full h-full object-cover"
-        />
-      </div>
+      {!isMobileScreen ? (
+        <div className="h-headerImage w-full">
+          <Image
+            src={agendaHeader}
+            alt="Cosette les Miserables ORPM"
+            style={{ objectPosition: '5% 35%' }}
+            className="w-full h-full object-cover"
+          />
+        </div>
+      ) : <></>}
       <div id="dates" className="lg:mb-120px mb-60px" />
       <div>
         <Typography.Title level={1} className="lg:mb-60px">
