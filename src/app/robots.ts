7b7201410abd9fbae0c7738ed154fa0d916cd28b
Louis Ledoux <19322697+louisledoux@users.noreply.github.com>
2024-09-18 15:16:38 +0200

    perf(seo): improve seo, disallow studio crawling

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: '/studio/',
     },
     sitemap: 'https://orpm-les-miserables.fr/sitemap.xml',
   };
