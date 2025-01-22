dde0bda9f00757509445181e2fff24126a52470c
Louis <19322697+louisledoux@users.noreply.github.com>
2025-01-22 19:09:39 +0100

    chore: lint

--- src/containers/Agenda/Agenda.tsx
+++ src/containers/Agenda/Agenda.tsx
@@ -1,8 +1,13 @@
 'use client';
 
-import AgendaItem, { AgendaItemProps } from '@/components/AgendaItem/AgendaItem';
+import AgendaItem, {
+  AgendaItemProps,
+} from '@/components/AgendaItem/AgendaItem';
 import { useEffect, useState } from 'react';
-import { getAgendaDatesFromFirebase, getPastDatesFromFirebase } from '@/services/agenda.service';
+import {
+  getAgendaDatesFromFirebase,
+  getPastDatesFromFirebase,
+} from '@/services/agenda.service';
 
 function Agenda() {
   const [currentDates, setCurrentDates] = useState<Array<AgendaItemProps>>([]);
@@ -26,28 +31,8 @@ function Agenda() {
       <section>
         <h2 className="text-3xl font-bold mb-8 text-center">Dates à venir</h2>
         <div className="flex flex-col md:flex-row gap-12 lg:gap-24 justify-center flex-wrap">
-          {currentDates.map(({
-            location, dateTime, partner, reservationLink, title,
-          }) => (
-            <AgendaItem
-              key={`${location}-${dateTime.getTime()}-${partner}`}
-              location={location}
-              dateTime={dateTime}
-              partner={partner}
-              reservationLink={reservationLink}
-              title={title}
-            />
-          ))}
-        </div>
-      </section>
-
-      {pastDates.length > 0 && (
-        <>
-          <hr className="w-full" />
-          <section>
-            <h2 className="text-3xl font-bold mb-8 text-center">Dates passées</h2>
-            <div className="flex flex-col md:flex-row gap-12 lg:gap-24 justify-center flex-wrap">
-            {pastDates.map(({
+          {currentDates.map(
+            ({
               location, dateTime, partner, reservationLink, title,
             }) => (
               <AgendaItem
@@ -57,9 +42,35 @@ function Agenda() {
                 partner={partner}
                 reservationLink={reservationLink}
                 title={title}
-                isPast
               />
-            ))}
+            ),
+          )}
+        </div>
+      </section>
+
+      {pastDates.length > 0 && (
+        <>
+          <hr className="w-full" />
+          <section>
+            <h2 className="text-3xl font-bold mb-8 text-center">
+              Dates passées
+            </h2>
+            <div className="flex flex-col md:flex-row gap-12 lg:gap-24 justify-center flex-wrap">
+              {pastDates.map(
+                ({
+                  location, dateTime, partner, reservationLink, title,
+                }) => (
+                  <AgendaItem
+                    key={`${location}-${dateTime.getTime()}-${partner}`}
+                    location={location}
+                    dateTime={dateTime}
+                    partner={partner}
+                    reservationLink={reservationLink}
+                    title={title}
+                    isPast
+                  />
+                ),
+              )}
             </div>
           </section>
         </>
